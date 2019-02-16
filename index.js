6b7dee1701d0fed6e2c8665b2ad53bdab2907047
Niklas Närhinen <niklas@narhinen.net>
2019-02-16 23:00:30 +0200

    Initial language files

---
+++ index.js
@@ -0,0 +1,5 @@
+import en from "./languages/en"
+
+export {
+  en
+}
---
+++ languages/en.js
@@ -0,0 +1,14 @@
+var lang = {
+  core: {
+    play: "Play",
+    pause: "Pause,
+    volume: "Volume",
+    mute: "Mute",
+    unmute: "Unmute",
+    fullscreen: "Fullscreen",
+    exit_fullscreen: "Exit fullscreen"
+  }
+}
+
+
+export default lang
