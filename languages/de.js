386d0b88eabbf760dcb7122a18ab668519b83367
okkonen <marko.okkonen@thirdpresence.com>
2019-04-02 15:52:18 +0300

    fix: make remaining languages available in translation plugin

---
+++ languages/de.js
@@ -0,0 +1,34 @@
+/**
+ * @type { import("./types").Translation}
+ */
+var en = {
+  audio: {
+    button_txt: "Ton",
+    menu_title: "Ton"
+  },
+  core: {
+    exit_fullscreen: "Vollbild verlassen",
+    fullscreen: "Vollbild",
+    mute: "Ton aus",
+    pause: "Pause",
+    play: "Abspielen",
+    unmute: "Ton ein",
+    volume: "Lautstärke"
+  },
+  playlist: {
+    cancel: "Abbrechen",
+    up_next: "Nächste"
+  },
+  qsel: {
+    menu_title: "Qualität"
+  },
+  share: {
+    clipboard_failure: "Zugriff auf Zwischenablage fehlgeschlagen",
+    clipboard_success: "Embedcode in die Zwischenablage kopiert",
+    embed: "Einbetten",
+    link: "Link",
+    menu_title: "Teilen"
+  }
+}
+
+module.exports = de
---
+++ languages/pt.js
@@ -0,0 +1,34 @@
+/**
+ * @type { import("./types").Translation}
+ */
+var en = {
+  audio: {
+    button_txt: "Áudio",
+    menu_title: "Áudio"
+  },
+  core: {
+    exit_fullscreen: "Tela não cheia",
+    fullscreen: "Tela cheia",
+    mute: "Silenciar",
+    pause: "Pausa",
+    play: "Reproduzir",
+    unmute: "Não silenciado",
+    volume: "Volume"
+  },
+  playlist: {
+    cancel: "Cancel",
+    up_next: "A seguir"
+  },
+  qsel: {
+    menu_title: "Qualidade"
+  },
+  share: {
+    clipboard_failure: "Access to clipboard failed",
+    clipboard_success: "O código de inserção está agora na sua área de transferência",
+    embed: "Embutir",
+    link: "Link",
+    menu_title: "Compartilhar"
+  }
+}
+
+module.exports = pt
