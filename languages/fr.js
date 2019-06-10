688d1b11df62e9663590c4af855749124f94d361
okkonen <marko.okkonen@thirdpresence.com>
2019-06-10 10:12:54 +0300

    feat: french language

---
+++ languages/fr.js
@@ -0,0 +1,40 @@
+/**
+ * @type { import("./types").Translation}
+ */
+var fr = {
+  audio: {
+    button_txt: "Audio",
+    menu_title: "Audio"
+  },
+  core: {
+    exit_fullscreen: "Quitter le mode plein écran",
+    fullscreen: "Plein écran",
+    mute: "Sourdine",
+    pause: "Pause",
+    play: "Jouer",
+    unmute: "Son activé",
+    volume: "Volume"
+  },
+  ovp: {
+    starting_in: "Compte à rebours"
+  },
+  playlist: {
+    cancel: "Annuler",
+    up_next: "Suivant"
+  },
+  qsel: {
+    menu_title: "Qualié"
+  },
+  share: {
+    clipboard_failure: "L'accès au presse-papier a échoué",
+    clipboard_success: "Le code embed est maintenant dans votre presse-papiers",
+    embed: "Embed",
+    link: "Lien",
+    menu_title: "Partager"
+  },
+  speed: {
+    menu_title: "Speed"
+  }
+}
+
+module.exports = fr
