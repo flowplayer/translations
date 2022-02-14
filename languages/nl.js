b34be2429fc5148d48ba167992fb8216a2299f30
Bert Verhelst <verhelstbert@gmail.com>
2022-02-14 13:46:09 +0100

    Feat(nl.js): add Dutch translation file

---
+++ languages/nl.js
@@ -0,0 +1,46 @@
+/**
+ * @type { import("./types").Translation}
+ */
+var nl = {
+  ads: {
+    ad: "Ad",
+    ads: "Ads",
+    advertisement: "Advertentie",
+    indicator: "Ads"
+  },
+  audio: {
+    button_txt: "Audio",
+    menu_title: "Audio"
+  },
+  core: {
+    exit_fullscreen: "Sluit fullscreen",
+    fullscreen: "Fullscreen",
+    mute: "Dempen",
+    pause: "Pause",
+    play: "Play",
+    unmute: "Dempen opheffen",
+    volume: "Volume"
+  },
+  ovp: {
+    starting_in: "Aftelling"
+  },
+  playlist: {
+    cancel: "Annuleren",
+    up_next: "Volgende"
+  },
+  qsel: {
+    menu_title: "Kwaliteit"
+  },
+  share: {
+    clipboard_failure: "Toegang tot klembord mislukt",
+    clipboard_success: "De tekst staat nu op je klembord",
+    embed: "Embed",
+    link: "Link",
+    menu_title: "Deel"
+  },
+  speed: {
+    menu_title: "Snelheid"
+  }
+}
+
+module.exports = nl
