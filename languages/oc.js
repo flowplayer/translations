337bd0455bcbbc206b889b5f43b953736a4b824d
Mejans <61360811+Mejans@users.noreply.github.com>
2023-02-22 18:05:47 +0100

    First translation into Occitan

---
+++ languages/oc.js
@@ -0,0 +1,46 @@
+/**
+ * @type { import("./types").Translation}
+ */
+var en = {
+  ads: {
+    ad: "Publicitat",
+    ads: "Publicitats",
+    advertisement: "Publicitat",
+    indicator: "Pubs"
+  },
+  audio: {
+    button_txt: "Àudio",
+    menu_title: "Àudio"
+  },
+  core: {
+    exit_fullscreen: "Sortir de l’ecran complèt",
+    fullscreen: "Ecran complèt",
+    mute: "Amudir",
+    pause: "Pausa",
+    play: "Lectura",
+    unmute: "Restablir lo son",
+    volume: "Volum"
+  },
+  ovp: {
+    starting_in: "Descompte"
+  },
+  playlist: {
+    cancel: "Anullar",
+    up_next: "Seguent"
+  },
+  qsel: {
+    menu_title: "Qualitat"
+  },
+  share: {
+    clipboard_failure: "Fracàs de l’accès al quichapapièrs",
+    clipboard_success: "Lo tèxt es copiat al quichapapièrs",
+    embed: "Integrar",
+    link: "Ligam",
+    menu_title: "Partejar"
+  },
+  speed: {
+    menu_title: "Velocitat"
+  }
+}
+
+module.exports = oc
