3d4c92da387a396e25fff7f717a2a1aeaac94004
Niklas Närhinen <niklas.narhinen@flowplayer.com>
2023-03-17 06:53:04 +0200

    chore: add oc to translations

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ import de from "./languages/de"
 import en from "./languages/en"
 import es from "./languages/es"
 import fi from "./languages/fi"
+import fr from "./languages/fr"
 import he from "./languages/he"
 import nb from "./languages/nb"
 import nl from "./languages/nl"
 import nn from "./languages/nn"
+import oc from "./languages/nl"
+import pl from "./languages/pl"
 import pt from "./languages/pt"
 import sk from "./languages/sk"
 import sv from "./languages/sv"
-import pl from "./languages/pl"
-import fr from "./languages/fr"
 import tr from "./languages/tr"
 
 export {
@@ -25,9 +26,10 @@ export {
   nb,
   nl,
   nn,
+  oc,
+  pl,
   pt,
   sk,
   sv,
-  pl,
   tr
 }
