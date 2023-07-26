b253f53ae2e3029dea212edc2a7c6b2081fc6c96
alexarassat <alexandros.satanakis@gmail.com>
2023-07-26 13:53:45 +0300

    esm modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,73 @@
 const fs = require("fs")
 const path = require("path")
 const LANGUAGES = [
-  "cs",
-  "de",
-  "en",
-  "es",
-  "fi",
-  "fr",
-  "he",
-  "nb",
-  "nl",
-  "nn",
-  "oc",
-  "pl",
-  "pt",
-  "sk",
-  "sv",
-  "tlh",
-  "tr"
+    "cs",
+    "de",
+    "en",
+    "es",
+    "fi",
+    "fr",
+    "he",
+    "nb",
+    "nl",
+    "nn",
+    "oc",
+    "pl",
+    "pt",
+    "sk",
+    "sv",
+    "tlh",
+    "tr"
 ]
 
 
 const tmp = lang => {
-  let name = path.join(__dirname, `tmp.${lang}.${Date.now()}.js`)
-    , fd = fs.openSync(name, "a+")
-  return { fd, name }
+    let name = path.join(__dirname, `tmp.${lang}.${Math.random()}.js`)
+        , fd = fs.openSync(name, "a+")
+    return {fd, name}
 }
 
-const entry = lang => {
-  let { fd, name } = tmp(lang)
-  fs.writeSync(fd, `flowplayer.i18n['${lang}'] = require('./languages/${lang}')`)
-  fs.close(fd)
-  return name
+const entry = (lang, is_esm) => {
+    let {fd, name} = tmp(lang)
+    fs.writeSync(fd, !is_esm
+        ? `flowplayer.i18n['${lang}'] = require('./languages/${lang}')`
+        :  `import {${lang}} from './index'; export default ${lang}`
+    )
+    fs.close(fd)
+    return name
 }
 
-const all = () => {
-  let { fd, name } = tmp("all")
-  fs.writeSync(fd, `import * as all from './index'; Object.assign(flowplayer.i18n, all)`)
-  fs.close(fd)
-  return name
+const all = (is_esm) => {
+    let {fd, name} = tmp("all")
+    fs.writeSync(fd, is_esm
+        ? `import * as all from './index'; export default all`
+        : `import * as all from './index'; Object.assign(flowplayer.i18n, all);`
+    )
+    fs.close(fd)
+    return name
 }
 
-const entries = Object.assign.apply({}, LANGUAGES.map(lang => ({ [`flowplayer.lang.${lang}`]: entry(lang)})))
+const entries_umd = Object.assign.apply({}, LANGUAGES.map(lang => ({[`flowplayer.lang.${lang}`]: entry(lang)})))
+const entries_esm = Object.assign.apply({}, LANGUAGES.map(lang => ({[`flowplayer.lang.${lang}`]: entry(lang, true)})))
 
-module.exports = {
-  entry: {
-    ...entries,
-    'flowplayer.lang.all': all()
-  }
+module.exports = [{
+    entry: {
+        ...entries_umd,
+        'flowplayer.lang.all': all()
+    }, output: {
+        path: path.resolve(__dirname, "dist")
+    }
+}, {
+    entry: {
+        ...entries_esm,
+        'flowplayer.lang.all': all(true)
+    }, output: {
+        path: path.resolve(__dirname, "dist/esm"),
+        library: {
+            type: "module"
+        }
+    }, experiments: {
+        outputModule: true
+    }
 }
+]
