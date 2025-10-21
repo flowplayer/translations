de482031d8141da72cadb27b259ceb7b94d628c5
Bernd Backhaus (Wowza) <bernd@stylebites.de>
2025-10-21 17:08:39 +0200

    Create hi.ts
    add hindi kanguage

---
+++ src/languages/hi.ts
@@ -0,0 +1,99 @@
+import { type Translation } from ".."
+
+export default {
+  ads: {
+    ad: "विज्ञापन",
+    ads: "विज्ञापन",
+    advertisement: "विज्ञापन",
+    indicator: "विज्ञापन"
+  },
+  audio: {
+    button_txt: "ऑडियो",
+    menu_title: "ऑडियो"
+  },
+  cc: {
+    button: "सीसी",
+    menu_title: "सबटाइटल्स",
+    options: "विकल्प",
+    tracks: "ट्रैक",
+    reset: "रीसेट",
+    colors: {
+      Black: "काला",
+      Blue: "नीला",
+      Cyan: "सियान",
+      Green: "हरा",
+      Magenta: "मैजेंटा",
+      Red: "लाल",
+      White: "सफ़ेद",
+      Yellow: "पीला",
+    },
+     edgeStyle: {
+      None: "कोई नहीं",
+      "Drop shadow": "छाया",
+      Raised: "ऊँचा",
+      Depressed: "नीचा",
+      Outline: "आउटलाइन"
+    },
+    fontFamily: {
+      "Monospaced Serif": "मोनोस्पेस्ड सेरिफ़",
+      "Proportional Serif": "प्रोर्पोर्शनल सेरिफ़",
+      "Monospaced Sans-Serif": "मोनोस्पेस्ड सैन्स-सेरिफ़",
+      "Proportional Sans-Serif": "प्रोर्पोर्शनल सैन्स-सेरिफ़",
+      Casual: "कैज़ुअल",
+      Cursive: "हस्तलिखित",
+    },
+    properties: {
+      fontFamily: "फ़ॉन्ट परिवार",
+      fontSize: "फ़ॉन्ट आकार",
+      fontColor: "फ़ॉन्ट रंग",
+      fontOpacity: "फ़ॉन्ट अस्पष्टता",
+      backgroundColor: "पृष्ठभूमि रंग",
+      backgroundOpacity: "पृष्ठभूमि अस्पष्टता",
+      characterEdgeStyle: "कैरेक्टर एज शैली",
+    }
+  },
+   chromecast: {
+    start: "कास्टिंग शुरू करें",
+    stop: "कास्टिंग रोकें"
+  },
+  core: {
+    exit_fullscreen: "फ़ुल स्क्रीन से बाहर निकलें",
+    fullscreen: "फ़ुल स्क्रीन",
+    mute: "म्यूट",
+    pause: "पॉज़",
+    play: "प्ले",
+    seconds: "सेकंड",
+    timeline_aria_label: "सीक स्लाइडर",
+    unmute: "अनम्यूट",
+    volume: "वॉल्यूम",
+    close: "बंद करें",
+    "skip-next": "अगला",
+    "skip-prev": "पिछला"
+  },
+  ovp: {
+    starting_in: "काउंटडाउन"
+  },
+playlist: {
+    cancel: "रद्द करें",
+    up_next: "अगला",
+    autoplay: "स्वचालित प्ले",
+    now_playing: "अभी प्ले हो रहा है",
+  },
+  qsel: {
+    menu_title: "गुणवत्ता"
+  },
+  share: {
+    clipboard_failure: "क्लिपबोर्ड तक पहुँच असफल",
+    clipboard_success: "पाठ अब आपके क्लिपबोर्ड पर है",
+    embed: "एम्बेड करें",
+    link: "लिंक",
+    menu_title: "साझा करें"
+  },
+  speed: {
+    menu_title: "गति"
+  },
+  vtsel: {
+    button_txt: "वीडियो",
+    menu_title: "वीडियो ट्रैक"
+  }
+} satisfies Translation
